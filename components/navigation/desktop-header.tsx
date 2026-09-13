9b35746b3a5f9a5309fd884af1e7eecd8c80ac79
Abdellah <khoudenak@gmail.com>
2026-09-13 04:30:57 +0100

    remove login from navbar

--- components/navigation/desktop-header.tsx
+++ components/navigation/desktop-header.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import Link from "next/link";
 import { Container } from "@/components/layout/container";
 import { PrimaryNavigation } from "@/components/navigation/primary-navigation";
-import { Button } from "@/components/ui/button";
 import { FplLogo } from "@/components/brand/fpl-logo";
 
 export function DesktopHeader() {
@@ -15,27 +13,6 @@ export function DesktopHeader() {
 
           {/* Primary Navigation */}
           <PrimaryNavigation />
-
-          {/* Action CTAs */}
-          <div className="flex items-center gap-3">
-            <Button
-              asChild
-              variant="glass"
-              size="sm"
-              className="rounded-full px-5 py-2 text-xs font-bold border border-white/20 hover:border-white/35"
-            >
-              <Link href="/admin">Log in</Link>
-            </Button>
-
-            <Button
-              asChild
-              variant="neon"
-              size="sm"
-              className="rounded-full px-5 py-2 text-xs font-black text-[#0B081E] shadow-[0_0_18px_rgba(0,255,163,0.35)] hover:shadow-[0_0_24px_rgba(0,255,163,0.5)]"
-            >
-              <Link href="/tournaments">Get started</Link>
-            </Button>
-          </div>
         </div>
       </Container>
     </header>
